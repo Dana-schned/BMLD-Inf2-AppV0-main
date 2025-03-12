8d820b634eeb7f91d8e1454dfa9de2a82a12fe3a
Katarina Djuric <djurickat@students.zhaw.ch>
2025-03-12 08:49:40 +0100

    neuer Data manager

--- Start.py
+++ Start.py
@@ -13,3 +13,16 @@ Diese App wurde von folgenden Personen entwickelt:
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 """
+
+from utils.data_manager import DataManager
+
+# initialize the data manager
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_ADHS")  # switch drive 
+
+# load the data from the persistent storage into the session state
+data_manager.load_app_data(
+    session_state_key='data_df', 
+    file_name='data.csv', 
+    initial_value = pd.DataFrame(), 
+    parse_dates = ['timestamp']
+    )
