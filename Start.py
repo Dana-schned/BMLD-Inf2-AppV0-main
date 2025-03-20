91f8f08612fdf9be5fb414facd6be266b0f208e8
Dana-schned <schned06@students.zhaw.ch>
2025-03-20 13:30:14 +0100

    Titel geändert, Buttons gemacht

--- Start.py
+++ Start.py
@@ -26,3 +26,30 @@ data_manager.load_app_data(
     initial_value = pd.DataFrame(), 
     parse_dates = ['timestamp']
     )
+
+# Seiten definieren
+pages = {
+   "Home": "Willkommen bei der AD(H)S App!",
+   "Rechner": "Hier ist der Rechner.",
+   "Wochenplaner": "Hier ist der Wochenplaner.",
+   "To-Do-Listen": "Hier sind die To-Do-Listen.",
+   "Tipps": "Hier sind hilfreiche Tipps."
+}
+# Aktuelle Seite aus Query-Parametern holen
+query_params = st.query_params
+if "page" in query_params:
+   st.session_state.current_page = query_params["page"] if isinstance(query_params["page"], str) else query_params["page"][0]
+else:
+   if "current_page" not in st.session_state:
+       st.session_state.current_page = "Home"
+# Navigationstitel
+st.title("Navigation")
+# Buttons für Seiten erstellen
+for page in pages.keys():
+   if st.button(page):
+       # Query-Parameter aktualisieren
+       st.query_params = {"page": page}
+       st.session_state.current_page = page
+# Inhalt der aktuellen Seite anzeigen
+st.subheader(st.session_state.current_page)
+st.write(pages[st.session_state.current_page])
